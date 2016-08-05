cc2d2b50b4d1513360999aa5b5bd22a1e824ccaf
bparrillo <robertparrillo2015@u.northwestern.edu>
2016-08-05 13:47:38 -0500

    delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -34,6 +34,13 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		flash[:notice] = "deleted"
+		redirect_to articles_path
+	end
 	
 	private
 		def article_params
